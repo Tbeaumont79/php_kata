a8f6eb5de107fcac5475d5deaae25dfbe9192061
tbeaumont79 <tbeaumont79@icloud.com>
2024-12-29 21:06:44 +0100

    feat: still working on basic loggin kata

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+    require("mydbConnect.php");
+
+    if(isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['name'];
+        $password = $_POST['password'];
+
+        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,5 +21,10 @@
     </head>
     <body>
         <h1>Php kata</h1>
+        <form action="index.php" method="post">
+            <input type="text" name="name" placeholder="name">
+            <input type="password" name="password" placeholder="password">
+            <input type="submit" value="Submit">
+        </form>
     </body>
 </html>
